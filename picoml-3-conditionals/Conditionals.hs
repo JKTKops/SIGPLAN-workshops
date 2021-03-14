da3e9112b2ac9bd6d36a346b961c82588d04156f
JKTKops <zerglingk9012@gmail.com>
2021-03-14 15:03:25 -0500

    code for 3/14

--- picoml-3-conditionals/Conditionals.hs
+++ picoml-3-conditionals/Conditionals.hs
@@ -1,9 +1,11 @@
-module Vars where
+module Conditionals where
 
 import Control.Applicative (liftA2)
 import Data.Functor (($>))
 import Text.Parsec hiding (parse)
 import qualified Text.Parsec as Parsec (parse)
+import Text.Parsec.Language
+import qualified Text.Parsec.Token as T
 import Text.Parsec.String
 
 --------------------------------------------------------------------------------------
@@ -20,6 +22,7 @@ data Statement
 data Exp
   = ConstExp Const
   | VarExp   String
+  | IfExp    Exp Exp Exp
   deriving (Eq, Show)
 
 data Const
@@ -69,47 +72,86 @@ unionEnv = (++)
 --
 --------------------------------------------------------------------------------------
 
+picomlDef :: LanguageDef st
+picomlDef = emptyDef
+  { T.commentStart = "(*"
+  , T.commentEnd   = "*)"
+  , T.reservedOpNames = ["=", ";;"]
+  , T.reservedNames =
+    [ "true", "false"
+    , "let"
+    , "if", "else", "then"
+    ]
+  }
+
+picoml :: T.TokenParser st
+picoml = T.makeTokenParser picomlDef
+
+lexeme, parens, brackets :: Parser a -> Parser a
+lexeme = T.lexeme picoml
+parens = T.parens picoml
+brackets = T.brackets picoml
+
+identifier, stringLiteral :: Parser String
+identifier    = T.identifier picoml
+stringLiteral = T.stringLiteral picoml
+
+reserved, reservedOp :: String -> Parser ()
+reserved   = T.reserved picoml
+reservedOp = T.reservedOp picoml
+
+whitespace :: Parser ()
+whitespace = T.whiteSpace picoml
+
 -- this will evolve as we add to the interpreter
 parse :: String -> Either ParseError Statement
-parse = Parsec.parse parseStmt "<interactive>"
+parse = Parsec.parse (between whitespace eof parseStmt) "<interactive>"
 
 parseStmt :: Parser Statement
 parseStmt = do
   stmt <- parseLetStmt <|> parseAnonStmt
-  _    <- string ";;"
+  _    <- reservedOp ";;"
   return stmt
 
 parseLetStmt :: Parser Statement
 parseLetStmt = do
-  _    <- try $ string "let"
-  _    <- spaces
-  name <- parseName
-  _    <- between spaces spaces $ char '='
+  _    <- reserved "let"
+  name <- identifier
+  _    <- reservedOp "="
   body <- parseExp
   return $ LetStmt name body
 
 parseAnonStmt :: Parser Statement
 parseAnonStmt = AnonStmt <$> parseExp
 
-parseName :: Parser String
-parseName = liftA2 (:) letter (many alphaNum)
-
 parseExp :: Parser Exp
 parseExp = ConstExp <$> parseConst
            <|> parseVar
+           <|> parseIf
+           <|> parens parseExp -- eventually parens parseAExp!
 
 parseConst :: Parser Const
-parseConst = (string "true" $> BoolConst True)
-             <|> (string "false" $> BoolConst False)
-             <|> (string "()" $> UnitConst)
+parseConst = (reserved "true" $> BoolConst True)
+             <|> (reserved "false" $> BoolConst False)
+             <|> (parens (pure ()) $> UnitConst)
              <|> parseNumber
              <|> parseString
 
 parseVar :: Parser Exp
-parseVar = VarExp <$> parseName
+parseVar = VarExp <$> identifier
+
+parseIf :: Parser Exp
+parseIf = do
+  _  <- reserved "if"
+  e1 <- parseExp
+  _  <- reserved "then"
+  e2 <- parseExp
+  _  <- reserved "else"
+  e3 <- parseExp
+  return $ IfExp e1 e2 e3
 
 parseNumber :: Parser Const
-parseNumber = do
+parseNumber = lexeme $ do
   integerPart <- many1 digit
   decimalPart <- optionMaybe (char '.' >> many digit)
   case decimalPart of
@@ -117,19 +159,11 @@ parseNumber = do
     Just d  -> return $ FloatConst $
                  read $ integerPart ++ "." ++ d ++ "0"
                  -- so we get 2.50, more importantly "2." => 2.0
+                 -- Parsec's 'float' parser doesn't do this, so we
+                 -- will keep doing it ourselves.
 
 parseString :: Parser Const
-parseString = StringConst <$> between (char '"') (char '"') parseStringContents
-
-parseStringContents :: Parser String
-parseStringContents = many $ noneOf ['"', '\\'] <|> parseEscapeChar
-  where
-    parseEscapeChar = do
-      character <- char '\\' >> oneOf ['"', '\\', 't']
-      return $ case character of
-        '"' -> '"'
-        '\\' -> '\\'
-        't'  -> '\t'
+parseString = StringConst <$> stringLiteral
 
 --------------------------------------------------------------------------------------
 --
@@ -143,6 +177,14 @@ parseStringContents = many $ noneOf ['"', '\\'] <|> parseEscapeChar
 typecheck :: Exp -> Gamma -> Maybe Type
 typecheck (ConstExp c) _      = Just $ typeof c
 typecheck (VarExp name) gamma = lookupEnv gamma name
+typecheck (IfExp e1 e2 e3) gamma = do
+  t1 <- typecheck e1 gamma
+  tau1 <- typecheck e2 gamma
+  tau2 <- typecheck e3 gamma
+  case t1 of
+    TyConst "bool"
+      | tau1 == tau2 -> Just tau1
+    _ -> Nothing
 
 typeof :: Const -> Type
 typeof IntConst{}    = TyConst "int"
@@ -168,6 +210,10 @@ evaluate (ConstExp c) env = case c of
 evaluate (VarExp name) env = case lookupEnv env name of
   Nothing -> error "evaluate VarExp: not in env (impossible!)"
   Just v  -> v
+
+evaluate (IfExp e1 e2 e3) env = case evaluate e1 env of
+  BoolVal True  -> evaluate e2 env
+  BoolVal False -> evaluate e3 env
 
 --------------------------------------------------------------------------------------
 --
